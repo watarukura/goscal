12ccc6fb85848fae010f39b8f9589072835aa9a1
Wataru Kurashima <what.r.j150@gmail.com>
2024-08-11 20:23:13 +0900

    feat: 🎸 chapter 3.4

--- main.go
+++ main.go
@@ -7,18 +7,22 @@ type Token interface {
 }
 type Ident string
 type Number string
+type LParen string
+type RParen string
 
 func (i Ident) getValue() interface{}  { return i }
 func (n Number) getValue() interface{} { return n }
+func (l LParen) getValue() interface{} { return l }
+func (r RParen) getValue() interface{} { return r }
 
 func main() {
-	input := "123 world"
+	input := "(123 456 world)"
 	fmt.Printf("source: %#v, parsed: %#v\n", input, source(input))
 
-	input = "Hello world"
+	input = "((car cdr) cdr)"
 	fmt.Printf("source: %#v, parsed: %#v\n", input, source(input))
 
-	input = "      world"
+	input = "()())))((()))"
 	fmt.Printf("source: %#v, parsed: %#v\n", input, source(input))
 }
 
@@ -58,6 +62,12 @@ func token(input string) (string, Token) {
 	if input, number := number(whitespace(input)); number != nil {
 		return input, number
 	}
+	if input, lparen := lparen(whitespace(input)); lparen != nil {
+		return input, lparen
+	}
+	if input, rparen := rparen(whitespace(input)); rparen != nil {
+		return input, rparen
+	}
 	return input, nil
 }
 
@@ -72,4 +82,23 @@ func source(input string) []Token {
 		}
 	}
 	return tokens
+}
+
+func lparen(input string) (string, Token) {
+	if len(input) > 0 && input[0] == '(' {
+		if len(input) > 1 {
+			return input[1:], LParen(input[0])
+		}
+		return "", LParen(input[0])
+	}
+	return input, nil
+}
+func rparen(input string) (string, Token) {
+	if len(input) > 0 && input[0] == ')' {
+		if len(input) > 1 {
+			return input[1:], RParen(input[0])
+		}
+		return "", RParen(input[0])
+	}
+	return input, nil
 }
