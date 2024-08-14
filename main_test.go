97d3e6cd0402bf6f8b0ce79eb98e82d2c41accfc
Wataru Kurashima <what.r.j150@gmail.com>
2024-08-15 08:42:35 +0900

    feat: 🎸 chapter 3.6

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 import "github.com/stretchr/testify/assert"
 
 func TestWhiteSpace(t *testing.T) {
@@ -9,16 +11,51 @@ func TestWhiteSpace(t *testing.T) {
 	})
 }
 func TestNumber(t *testing.T) {
-	t.Run("number", func(t *testing.T) {
-		i, num := number("123.45 ")
-		assert.Equal(t, " ", i)
-		assert.Equal(t, Number("123.45"), num)
-	})
+	testCases := []struct {
+		name string
+		arg  string
+		want Expression
+	}{
+		{name: "valid", arg: "123.45 ", want: Number(123.45)},
+		{name: "not num", arg: "aaa", want: Expression(nil)},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			_, actual, _ := number(test.arg)
+			assert.Equal(t, test.want, actual)
+		})
+	}
 }
 func TestIdent(t *testing.T) {
-	t.Run("ident", func(t *testing.T) {
-		i, ident := ident("Adam")
-		assert.Equal(t, "", i)
-		assert.Equal(t, Ident("Adam"), ident)
-	})
+	testCases := []struct {
+		name string
+		arg  string
+		want Expression
+	}{
+		{name: "valid", arg: "Adam ", want: Ident("Adam")},
+		{name: "not identifier", arg: "123", want: Expression(nil)},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			_, actual, _ := ident(test.arg)
+			assert.Equal(t, test.want, actual)
+		})
+	}
+}
+func TestExpression(t *testing.T) {
+	testCases := []struct {
+		name string
+		arg  string
+		want Expression
+	}{
+		{name: "valid ident", arg: "Adam", want: Ident("Adam")},
+		{name: "valid num", arg: "123", want: Number(123)},
+		{name: "valid add", arg: "123.4 + 234.5", want: Add{Number(123.4), Number(234.5)}},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			_, actual, _ := expr(test.arg)
+			assert.Equal(t, test.want, actual)
+		})
+	}
 }
